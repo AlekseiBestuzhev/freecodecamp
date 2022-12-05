4b4379e349ae45b9593973fb1c61a8244d535374
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-12-05 16:14:15 +0300

    update: js_basics task_07 complete

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 // It's a comment
 /* It's also 
 a comment */
-var myName;
-let a;
-a = 7;
-let b;
-b = a;
-let c = 9;
-var myFirstName = 'Alex';
-var myLastName = 'Alex';
+// var myName;
+// let a;
+// a = 7;
+// let b;
+// b = a;
+// let c = 9;
+// var myFirstName = 'Alex';
+// var myLastName = 'Alex';
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = 'I am a';
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
