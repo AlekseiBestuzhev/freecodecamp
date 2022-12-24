bcff406869bd4aaa74b24bf52196cbd211338ff4
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-12-24 16:30:15 +0400

    update: js_basics task_75 done

--- java-script/script.js
+++ java-script/script.js
@@ -434,5 +434,25 @@ function golfScore(par, strokes) {
 	}
 	return "Change Me";
 }
-
 golfScore(5, 4);
+
+
+function caseInSwitch(val) {
+	let answer = "";
+	switch (val) {
+		case 1:
+			return 'alpha';
+			break;
+		case 2:
+			return 'beta';
+			break;
+		case 3:
+			return 'gamma';
+			break;
+		case 4:
+			return 'delta';
+			break;
+	}
+	return answer;
+}
+caseInSwitch(1);
