b0a936386737cd12bc422d25cf51b506c3133b60
Alexksxx <bestuzhev.aleksei.m@gmail.com>
2022-12-25 15:41:15 +0400

    update: js_basics task_81 done

--- java-script/script.js
+++ java-script/script.js
@@ -542,3 +542,36 @@ function abTest80(a, b) {
 }
 const a80 = abTest80(-2, 2);
 console.log(a80);
+
+
+let count = 0;
+function cc(card) {
+	// Only change code below this line
+	switch (card) {
+		case 10:
+		case 'J':
+		case 'Q':
+		case 'K':
+		case 'A':
+			count -= 1;
+			break;
+		case 7:
+		case 8:
+		case 9:
+			count += 0;
+			break;
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			count += 1;
+			break;
+	}
+	let a81;
+	if (count <= 0) a81 = ' Hold';
+	else a81 = ' Bet';
+	return count + a81;
+	// Only change code above this line
+}
+cc(2); cc(3); cc(7); cc('K'); cc('A');
